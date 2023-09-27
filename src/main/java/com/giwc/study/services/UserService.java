757fe050a7ee9e8993d47fc78fe5cef64ce38374
Gabriel <wenck13@gmail.com>
2023-09-27 12:07:06 -0300

    Get User by Id

--- src/main/java/com/giwc/study/services/UserService.java
+++ src/main/java/com/giwc/study/services/UserService.java
@@ -2,10 +2,12 @@ package com.giwc.study.services;
 
 import com.giwc.study.domain.User;
 import com.giwc.study.repository.UserRepository;
+import com.giwc.study.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
