c3ebef4ab4d0b46acba18e8a0651f8ae3a30f42f
Gabriel <wenck13@gmail.com>
2023-09-26 21:04:34 -0300

    Connecting to MongoDB

---
+++ src/main/java/com/giwc/study/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.giwc.study.repository;
+
+import com.giwc.study.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/giwc/study/services/UserService.java
@@ -0,0 +1,19 @@
+package com.giwc.study.services;
+
+import com.giwc.study.domain.User;
+import com.giwc.study.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
