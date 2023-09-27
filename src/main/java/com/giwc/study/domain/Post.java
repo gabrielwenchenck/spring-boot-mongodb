7590a349c5921ac3d0bef41e760858474d3a2b6c
Gabriel <wenck13@gmail.com>
2023-09-27 19:05:25 -0300

    Add comments to posts

--- src/main/java/com/giwc/study/domain/Post.java
+++ src/main/java/com/giwc/study/domain/Post.java
@@ -2,12 +2,15 @@ package com.giwc.study.domain;
 
 
 import com.giwc.study.dto.AuthorDTO;
+import com.giwc.study.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -22,6 +25,8 @@ public class Post implements Serializable {
     private String body;
 
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -72,6 +77,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
