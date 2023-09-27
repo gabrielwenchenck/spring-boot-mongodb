7f0da76b7762f0596832f8753cc5e2a80eead721
Gabriel <wenck13@gmail.com>
2023-09-27 18:26:35 -0300

    Get User posts

--- src/main/java/com/giwc/study/resources/UserResource.java
+++ src/main/java/com/giwc/study/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.giwc.study.resources;
 
+import com.giwc.study.domain.Post;
 import com.giwc.study.domain.User;
 import com.giwc.study.dto.UserDTO;
 import com.giwc.study.services.UserService;
@@ -52,6 +53,13 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = {"/{id}/posts"})
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 
