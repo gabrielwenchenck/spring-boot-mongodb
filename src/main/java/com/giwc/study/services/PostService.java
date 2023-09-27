ac022faf05d368fed3b3697788b82d65c7dccf50
Gabriel <wenck13@gmail.com>
2023-09-27 19:48:02 -0300

    @Query search

--- src/main/java/com/giwc/study/services/PostService.java
+++ src/main/java/com/giwc/study/services/PostService.java
@@ -23,7 +23,10 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 
+    //    public List<Post> findByTitle(String text) {
+//        return repository.findByTitleContainingIgnoreCase(text);
+//    }
     public List<Post> findByTitle(String text) {
-        return repository.findByTitleContainingIgnoreCase(text);
+        return repository.searchTitle(text);
     }
 }
